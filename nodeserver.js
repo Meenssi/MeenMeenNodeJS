2aec17a5a36234c98e1aec58da9270b5527dba74
Meenssi <fumikoe439@gmail.com>
2022-04-21 10:02:21 +0700

    update nodeserver.js

--- nodeserver.js
+++ nodeserver.js
@@ -11,9 +11,27 @@ var user_list = [
     { name: "Annie", address: "PKT", birth_year: 1999},
     { name: "Winner", address: "CNX", birth_year: 2022}
 ];
+
+//Add Feature Bar Variables
+var feature = [
+    {img : "/img/AIot.svg",
+    top : "Programing Courses",
+    bottom : "Enjoy Coding and Playing HTML, CSS, JavaScripts <^v^>"},
+
+    {img : "/img/Programing.svg",
+    top : "AIoT & Robotics Courses",
+    bottom : "Enjoy Coding and Playing Python, ROS, Mechanict <^v^>"}
+]
+
+//Add content Fonts Variables
+var contentfont1 ="ONLINE CREATIVE WEBSITE"
+var contentfont2 = "Presented By MeenMeen"
  
-//Set &amp; Call EJS
+//Set & Call EJS
 app.set('view engine','ejs')
+
+//Connect public folder
+app.use(express.static('public'))
  
 //Back-End NodeJS Display
 app.get("/hello",(req,res) =>{
@@ -24,6 +42,13 @@ app.get("/hello",(req,res) =>{
 app.get("/",(req,res) =>{
     res.render('index', {userid : id, status : status, 
         obj_user_list : user_list})
+})
+
+//Connect index2.ejs
+app.get("/index2",(req,res) =>{
+    res.render('index2', {obj_feature : feature,
+        contentfont1 : contentfont1,
+        contentfont2 : contentfont2})
 })
  
 //Open Server
